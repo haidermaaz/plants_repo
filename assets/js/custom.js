51e16487fc25c3da5160329f70dd10b9b837a92b
maaz haider <haidermaaz29@gmail.com>
2023-06-12 12:21:59 +0500

    ok

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,8 @@
+function load(){
+    alert("It contains 2 web pages first one is: 'SERVICES' & second is: 'PROJECTS'. I done this task assigned by my software house to convert Figma design into HTML  ")
+}
+
+
 function drop(){
     var element = document.getElementById("d-men")
     element.classList.toggle("hidden"); 
